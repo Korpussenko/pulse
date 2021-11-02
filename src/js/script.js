ff802e35ee85ae48f99a953bf3810cd4af35b5d7
Yevgeniy Korpussenko <e.korpousenko@gmail.com>
2021-11-02 12:54:25 +0100

    Complete catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,13 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
 });
+
+$(document).ready(function() {
+ 
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+});
