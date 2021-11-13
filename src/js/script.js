71aaa38706ddc3c52819405ba6423c19cf0c4f9a
Yevgeniy Korpussenko <e.korpousenko@gmail.com>
2021-11-13 01:08:51 +0100

    Add modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -37,4 +37,24 @@ $(document).ready(function() {
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
 
+	//Modal
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+
+	$('.modal__close').on('click', function(){
+		$('.modal, .overlay').fadeOut('slow');
+	});
+
+	$('.button_catalog').on('click', function(){
+		$('.overlay, #order').fadeIn('slow');
+	});
+
+	$('.button_catalog').each(function(i) {
+		$(this).on('click',function(){
+			$('#order .modal__desc').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
 });
